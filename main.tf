dc6a1b6f12e00eaafc7ef4b48fc84dcec0c2b489
Mark Lightfoot <mark.lightfoot@cloudsolutions.co.uk>
2021-04-29 13:33:20 +0100

    fix: str vs tuple

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "google_compute_forwarding_rule" "default" {
   subnetwork            = data.google_compute_subnetwork.network.self_link
   allow_global_access   = var.global_access
   load_balancing_scheme = "INTERNAL"
-  backend_service       = var.create_backends == false ? var.backend_service_sl : google_compute_region_backend_service.default[*].self_link
+  backend_service       = var.create_backends == false ? var.backend_service_sl : google_compute_region_backend_service.default[0].self_link
   ip_address            = var.ip_address
   ip_protocol           = var.ip_protocol
   ports                 = var.all_ports == true ? null : var.ports
